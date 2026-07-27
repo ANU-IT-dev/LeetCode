0c6302f11d5113e90989b1721ec6bbcf36099431
ANU-IT-dev <anupriaa30@gmail.com>
2026-07-27 12:42:13 +0530

    Time: 52 ms (71.14%), Space: 71.1 MB (74.03%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,7 +1,73 @@
 class Solution {
 public:
-    vector<int> sortArray(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+    void merge(vector<int>&arr, int l,  int m ,int r)
+    {
+        int n1= m-l+1;
+        int n2= r-m;
+        int L[n1] ,R[n2];
+        int k=l;
+        // fill L
+
+        for(int i=0; i<n1; i++)
+        {
+            L[i]=arr[k];
+            k++;
+        }
+
+        // fill R
+        for(int i=0; i<n2; i++)
+        {
+            R[i]=arr[k];
+            k++;
+        }
+
+        //merge them in arr;
+         int i=0; int j=0; k=l;
+         while(i<n1 && j<n2)
+         {
+            if(L[i]<=R[j])
+            {
+                arr[k]=L[i];
+                i++;
+            }
+            else
+            {
+                arr[k]=R[j];
+                j++;
+
+            }
+            k++;
+         }
+         while(i<n1)
+         {
+            arr[k]= L[i];
+            i++;
+            k++;
+         }
+         while(j<n2)
+         {
+            arr[k]= R[j];
+            j++;
+            k++;
+         }
+    }
+    void mergesort(vector<int>&arr, int l,int r)
+    {
+        if(l>=r)
+        {
+            return;
+
+        }
+        int mid = l+(r-l)/2;
+       mergesort(arr,l,mid);
+       mergesort(arr,mid+1 ,r);
+
+        merge(arr,l,mid,r);
+    }
+
+    vector<int> sortArray(vector<int>& nums)
+    {
+        mergesort(nums, 0, nums.size() - 1);
         return nums;
     }
 };
